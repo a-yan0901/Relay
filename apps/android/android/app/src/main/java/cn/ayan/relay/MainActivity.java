53d2e3887f5549ffc6adcc99ca8d2993365ee386
weichao <weichao@fusionmind.ai>
2026-09-18 12:34:23 +0800

    refactor: use android back dispatcher

--- apps/android/android/app/src/main/java/cn/ayan/relay/MainActivity.java
+++ apps/android/android/app/src/main/java/cn/ayan/relay/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.ayan.relay;
 
+import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -7,18 +8,29 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(android.os.Bundle savedInstanceState) {
         registerPlugin(RelayNativePlugin.class);
         super.onCreate(savedInstanceState);
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                dispatchRelayBack(this);
+            }
+        });
     }
 
-    @Override
-    public void onBackPressed() {
+    private void dispatchRelayBack(OnBackPressedCallback callback) {
         if (getBridge() == null || getBridge().getWebView() == null) {
-            super.onBackPressed();
+            callback.setEnabled(false);
+            getOnBackPressedDispatcher().onBackPressed();
+            callback.setEnabled(true);
             return;
         }
         getBridge().getWebView().evaluateJavascript(
             "(function(){var event=new Event('relay:back',{cancelable:true});window.dispatchEvent(event);return event.defaultPrevented;})()",
             handled -> {
-                if (!"true".equals(handled)) MainActivity.super.onBackPressed();
+                if (!"true".equals(handled)) {
+                    callback.setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                    callback.setEnabled(true);
+                }
             }
         );
     }
